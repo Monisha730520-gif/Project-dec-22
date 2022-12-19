0c29822ed62d143a66d0c50be99506bc2d153091
Monisha730520-gif <monitom234@gmail.com>
2022-12-19 20:26:40 +0530

    sample project

---
+++ src/test/java/org/test/Project_dec_22/SampleProject.java
@@ -0,0 +1,37 @@
+package org.test.Project_dec_22;
+
+
+
+public class SampleProject {
+
+	public static void main(String[] args) {
+
+		for(int i=9;i>5;i--) {
+			System.out.println(i);
+		}
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+		}
+		for(int i=0;i<5;i=i+2) {
+			System.out.println(i);
+		
+		}
+		for(int i=0;i<3;i++) {
+			
+			for(int j=0;j<4;j++) {
+			System.out.println(j);	
+				
+			}
+			System.out.println(i);
+			System.out.println();
+		}
+		
+		
+	}
+
+
+
+
+	}
+
+
